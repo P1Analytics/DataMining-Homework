d152a771d9a726d05c71d0848df274deff481366
Simone90SSL <simone_ro@hotmail.it>
2016-10-21 11:50:08 +0200

    First exercise: compute some funny permutations over a deck.  TODO: point d and e

---
+++ exercise_1/util.py
@@ -0,0 +1,28 @@
+import math
+
+seeds = ['h','c','d','s']
+number = [x for x in range(1, 14)]
+cards = [(n, seed) for n in number
+         for seed in seeds]
+
+def getFirstXCards(x, res, notAllowedCards=[]):
+    if x is 0:
+        res.append([x for x in notAllowedCards])
+        return
+    for card in cards:
+        if card not in notAllowedCards:
+            notAllowedCards.append(card)
+            getFirstXCards(x - 1, res, notAllowedCards)
+            notAllowedCards.remove(card)
+        else:
+            continue
+
+def get_combination(a, b):
+    if a < b:
+        return 0
+    return math.factorial(a)/(math.factorial(b)*math.factorial(a-b))
+
+def fact(n):
+    if n <= 0:
+        return 0
+    return math.factorial(n)
